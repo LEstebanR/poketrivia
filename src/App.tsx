06461e9a5cadd47c277c5d4d4ef1ceb6883daa68
Luis Esteban Ramírez <leramirezca@gmail.com>
2022-05-27 22:41:07 -0500

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import './App.css'
+import './App.css';
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+} from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const App = () => {
-
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} position={"bottom-right"} />
       <h1>PokeTrivia</h1>
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
